09886c627bdfc9905deb25832d96dadde45bfcaa
uo285032 <113892398+uo285032@users.noreply.github.com>
2025-02-12 13:13:45 +0100

    Nombre aplicación spring actualizado

--- src/main/java/com/uniovi/WiqEs04bApplication.java
+++ src/main/java/com/uniovi/WiqEs04bApplication.java
@@ -6,7 +6,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class WiqEs04bApplication {
+public class WichatEs4bApplication {
     public static void main(String[] args) {
         SpringApplication.run(WiqEs04bApplication.class, args);
     }
