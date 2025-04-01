75889ca111ad85d1847f068a742b0374a1ba8136
manugbd <garciabaldo04@gmail.com>
2025-04-01 13:18:21 +0200

    Refactorización de las clases QuestionGenerator--Service para evitar la duplicación de código.

--- src/main/java/com/uniovi/services/QuestionGeneratorService.java
+++ src/main/java/com/uniovi/services/QuestionGeneratorService.java
@@ -1,7 +1,5 @@
 package com.uniovi.services;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.uniovi.components.generators.QuestionGenerator;
 import com.uniovi.components.generators.QuestionGeneratorV2;
 import com.uniovi.dto.QuestionDto;
@@ -9,110 +7,45 @@ import com.uniovi.entities.Answer;
 import com.uniovi.entities.Category;
 import com.uniovi.entities.Language;
 import com.uniovi.entities.Question;
+import com.uniovi.services.abstracts.AbstractQuestionGeneratorService;
 import com.uniovi.services.impl.QuestionServiceImpl;
 import jakarta.transaction.Transactional;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Deque;
 import java.util.List;
 
 @Service
-public class QuestionGeneratorService {
-
-    private final QuestionServiceImpl questionService;
+public class QuestionGeneratorService extends AbstractQuestionGeneratorService<QuestionServiceImpl> {
 
     public static final String JSON_FILE_PATH = "static/JSON/QuestionTemplates.json";
 
-    private final Deque<QuestionType> types = new ArrayDeque<>();
-
-    private JsonNode json;
-
-    private final Environment environment;
-
-    private final Logger log = LoggerFactory.getLogger(QuestionGeneratorService.class);
-
-    private boolean started;
-
     public QuestionGeneratorService(QuestionServiceImpl questionService, Environment environment) throws IOException {
-        this.questionService = questionService;
-        this.environment = environment;
-        ((QuestionServiceImpl)questionService).setQuestionGeneratorService(this);
-        parseQuestionTypes();
-        this.started = true;
+        super(questionService, environment, JSON_FILE_PATH);
     }
 
-    private void parseQuestionTypes() throws IOException {
-        if (json == null) {
-            Resource resource = new ClassPathResource(JSON_FILE_PATH);
-            ObjectMapper objectMapper = new ObjectMapper();
-            json = objectMapper.readTree(resource.getInputStream());
-        }
-        JsonNode categories = json.findValue("categories");
-        for (JsonNode category : categories) {
-            String categoryName = category.get("name").textValue();
-            Category cat = new Category(categoryName);
-            JsonNode questionsNode = category.findValue("questions");
-            for (JsonNode question : questionsNode) {
-                types.push(new QuestionType(question, cat));
-            }
-        }
+    @Override
+    protected void setQuestionGeneratorService() {
+        questionService.setQuestionGeneratorService(this);
     }
 
-    public void generateAllQuestions() throws IOException {
-        started = true;
-        resetGeneration();
-    }
-
+    @Override
     @Transactional
-    public void generateQuestions() throws IOException, InterruptedException {
-        if (types.isEmpty()) {
-            return;
-        }
-
-        if (started) {
-            started = false;
-            questionService.deleteAllQuestions();
-        }
-
-        if (Arrays.stream(environment.getActiveProfiles()).anyMatch(env -> (env.equalsIgnoreCase("test")))) {
-            log.info("Test profile active, skipping sample data insertion");
-            return;
-        }
-
+    public void processQuestions() throws IOException, InterruptedException {
         QuestionGenerator qgen = new QuestionGeneratorV2(json);
-        do{
+        do {
             QuestionType type = types.pop();
-            List<QuestionDto> questions;
-
-            List<Question> qsp = qgen.getQuestions(Language.ES.getCode(), type.getQuestion(), type.getCategory());
-            questions = qsp.stream().map(QuestionDto::new).toList();
-            questions.forEach(questionService::addNewQuestion);
-
-            List<Question> qen = qgen.getQuestions(Language.EN.getCode(),  type.getQuestion(), type.getCategory());
-            questions = qen.stream().map(QuestionDto::new).toList();
-            questions.forEach(questionService::addNewQuestion);
-
-            List<Question> qfr = qgen.getQuestions(Language.FR.getCode(),  type.getQuestion(), type.getCategory());
-            questions = qfr.stream().map(QuestionDto::new).toList();
-            questions.forEach(questionService::addNewQuestion);
-
-            List<Question> qDe = qgen.getQuestions(Language.DE.getCode(),  type.getQuestion(), type.getCategory());
-            questions = qDe.stream().map(QuestionDto::new).toList();
-            questions.forEach(questionService::addNewQuestion);
-        }while (!types.isEmpty());
-
+            for (Language lang : List.of(Language.ES, Language.EN, Language.FR, Language.DE)) {
+                List<Question> questions = qgen.getQuestions(lang.getCode(), type.getQuestion(), type.getCategory());
+                questions.stream()
+                        .map(QuestionDto::new)
+                        .forEach(questionService::addNewQuestion);
+            }
+        } while (!types.isEmpty());
     }
 
+    @Override
     @Transactional
     public void generateTestQuestions() throws IOException, InterruptedException {
         QuestionGenerator qgen = new QuestionGeneratorV2(json);
@@ -124,31 +57,12 @@ public class QuestionGeneratorService {
         questions.forEach(questionService::addNewQuestion);
     }
 
+    @Override
     @Transactional
     public void generateTestQuestions(String cat) {
         Answer a1 = new Answer("1", true);
         List<Answer> answers = List.of(a1, new Answer("2", false), new Answer("3", false), new Answer("4", false));
         Question q = new Question("Statement", answers, a1, new Category(cat), Language.ES);
         questionService.addNewQuestion(new QuestionDto(q));
-    }
-
-    public void setJsonGeneration(JsonNode json) {
-        this.json = json;
-    }
-
-    public void resetGeneration() throws IOException {
-        types.clear();
-        parseQuestionTypes();
-    }
-
-    public JsonNode getJsonGeneration() {
-        return json;
-    }
-
-    @Getter
-    @AllArgsConstructor
-    private static class QuestionType {
-        private final JsonNode question;
-        private final Category category;
     }
 }
