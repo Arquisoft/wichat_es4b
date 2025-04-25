4ae2e838e68d564c67f1df6df446386ab7d87d5e
NicolasGuerbartchoukPerez <nicolas.guerbartchouk@gmail.com>
2025-04-25 18:54:26 +0200

    Se han añadido ya funcional en ejecución automatizada los test cucumber de integración de los dos controladores de juego.

--- src/test/java/com/uniovi/CucumberRunnerTests.java
+++ src/test/java/com/uniovi/CucumberRunnerTests.java
@@ -4,9 +4,23 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"}, features = "src/test/resources/features")
+@CucumberOptions(
+        plugin = {"pretty"},
+        features = {
+                "src/test/resources/features/home_page.feature",
+                "src/test/resources/features/loginForm.feature",
+                "src/test/resources/features/player_data_modify.feature",
+                "src/test/resources/features/signUpForm.feature"
+                // NO incluyas la del juego aquí
+        }
+)
 @SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+@ActiveProfiles("test")
 public class CucumberRunnerTests {
 }
+
